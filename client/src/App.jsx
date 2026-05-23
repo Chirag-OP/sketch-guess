3e1626b87993d3114aaf2c607d9a26a3977be683
Chirag-OP <tayalchirag02@gmail.com>
2026-05-23 23:51:49 +0530

    Implement freehand canvas drawing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,11 @@ import { use } from 'react'
 function App() {
   const [value, setValue] = useState('a')
   const [mess, setMess] = useState('');
-  const socketRef = useRef(null)
+  const socketRef = useRef(null);
+
+  const canvasRef=useRef(null);
+  const ctxRef=useRef(null);
+  const isDrawingRef=useRef(false);
   useEffect(() => {
     fetch('http://localhost:3000/api')
     .then((res)=>{
@@ -21,6 +25,20 @@ function App() {
     socketRef.current.on("chat",(arg)=>{
       setValue(arg);
     })
+    
+    const canvas=canvasRef.current;
+
+    const ratio=window.devicePixelRatio;
+    canvas.width=1000 * ratio;
+    canvas.height=500 * ratio;
+    canvas.style.width=`${1000}px`;
+    canvas.style.height=`${500}px`;
+    
+    const ctx=canvas.getContext('2d');
+    ctx.scale(ratio,ratio);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth=5;
+    ctxRef.current=ctx;
     return () => {
     socketRef.current?.disconnect()
   }
@@ -33,12 +51,35 @@ function App() {
   function handleChange(e){
     setMess(e.target.value)
   }
+  function startDrawing(e){
+    const {offsetX, offsetY}=e.nativeEvent;
+    isDrawingRef.current=true;
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(offsetX,offsetY);
+  }
+  function finishDrawing(e){
+    ctxRef.current.closePath();
+    isDrawingRef.current=false;
+  }
+  function draw(e){
+    if(!isDrawingRef.current) return;
+    const {offsetX, offsetY}=e.nativeEvent;
+    ctxRef.current.lineTo(offsetX,offsetY);
+    ctxRef.current.stroke();
+  }
+  function clearDrawing(){
+    ctxRef.current.clearRect(0,0,1000,500);
+  }
   return (
     <>
       <section>
-        <div><input type="text" onChange={handleChange} value={mess}/></div>
-        <div><button onClick={handleClick}>Click Me</button></div>
-        <div>{value}</div>
+        <div>
+          <canvas onMouseDown={startDrawing} onMouseUp={finishDrawing} onMouseMove={draw} ref={canvasRef}></canvas>
+          <button onClick={clearDrawing} className='bg-amber-200 rounded-lg shadow-xs hover:translate-x-0.5 hover:translate-y-0.5 transition-all duration-300'> Clear </button>
+          <div><input type="text" onChange={handleChange} value={mess}/></div>
+          <div><button onClick={handleClick} className='bg-amber-200 rounded-lg shadow-xs hover:translate-x-0.5 hover:translate-y-0.5 transition-all duration-300'>Click Me</button></div>
+          <div>{value}</div>
+        </div>
       </section>
     </>
   )
